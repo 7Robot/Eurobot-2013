aebddca1c728f22fe4023c5d1697baa9fe2ec191
DIB Guillaume <guillaume.dib@gmail.com>
2013-03-13 15:47:25 +0100

    Ajout du projet Asserv

---
+++ Asserv.X/AsservHeader.h
@@ -0,0 +1,43 @@
+/*
+* Template dsPIC33F
+* Compiler : Microchip xC16
+* µC : 33FJ64MC802
+* Juillet 2012
+*    ____________      _           _
+*   |___  /| ___ \    | |         | |
+*      / / | |_/ /___ | |__   ___ | |_
+*     / /  |    // _ \| '_ \ / _ \| __|
+*    / /   | |\ \ (_) | |_) | (_) | |_
+*   /_/    |_| \_\___/|____/ \___/'\__|
+*			      7robot.fr
+*/
+
+/******************************************************************************/
+/* System Level #define Macros                                                */
+/******************************************************************************/
+
+/* TODO Define system operating frequency */
+
+/* Microcontroller MIPs (FCY) */
+#define SYS_FREQ        80000000 //7370000L
+#define FCY             SYS_FREQ/2
+
+/******************************************************************************/
+/* User Level #define Macros                                                  */
+/******************************************************************************/
+
+/* TODO Application specific user parameters used in user.c may go here */
+
+#define led _LATA0
+
+/******************************************************************************/
+/* Function Prototypes                                                        */
+/******************************************************************************/
+
+/* TODO User level functions prototypes (i.e. InitApp) go here */
+
+void ConfigureOscillator(void);    /* Handles clock switching/osc initialization */
+
+void InitApp(void);             /* I/O and Peripheral Initialization          */
+
+
