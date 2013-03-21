e3ed300df205d7fca7d9588c3d5198af1b1ab90f
Thomas Nicot <tomico87@hotmail.fr>
2013-03-21 22:20:55 +0100

    Code de la balise d'émission de Quentin

---
+++ balisesIR/beacon.X/main.c
@@ -0,0 +1,110 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+/* Device header file */
+#if defined(__dsPIC33E__)
+	#include <p33Exxxx.h>
+#elif defined(__dsPIC33F__)
+	#include <p33Fxxxx.h>
+#endif
+
+#include <stdint.h>        /* Includes uint16_t definition                    */
+#include <stdbool.h>       /* Includes true/false definition                  */
+
+#include "system.h"        /* System funct/params, like osc/peripheral config */
+#include "user.h"
+/* User funct/params, such as InitApp              */
+
+/******************************************************************************/
+/* Global Variable Declaration                                                */
+/******************************************************************************/
+
+/* i.e. uint16_t <variable_name>; */
+
+/******************************************************************************/
+/* Main Program                                                               */
+/******************************************************************************/
+#include <uart.h>
+#include <pwm.h>
+#include <ports.h>
+#include <adc.h>
+#include <libpic30.h>
+#include <pwm12.h>
+#include <libpic30.h>
+#include <timer.h>
+
+volatile unsigned i = 1;
+unsigned int config1;
+unsigned int config2;
+unsigned int config3;
+unsigned int U1MODEvalue;
+unsigned int U1STAvalue;
+unsigned int baud;
+unsigned int sptime;
+unsigned int period;
+
+
+int16_t main(void)
+{
+
+    /* Configure the oscillator for the device */
+    ConfigureOscillator();
+
+    /* Initialize IO ports and peripherals */
+    InitApp();
+
+    AD1PCFGL = 0xFFFF; //Putain, met ça en numérique, quoi !
+
+    TRISAbits.TRISA0 = 0; // Microstick LED
+    LATAbits.LATA0 = 1;
+
+    TRISBbits.TRISB4 = 1;
+    
+
+     __builtin_write_OSCCONL(OSCCON & 0xBF); // Unlock registers.
+    RPOR3bits.RP6R = 3; // U1TX sur RP6
+    __builtin_write_OSCCONL(OSCCON | 0x40); // Relock registers.
+    
+    ConfigIntCN(CHANGE_INT_ON & CHANGE_INT_PRI_4 & 0xFF000002); /*Interrupt sur CN1*/
+    CNEN2 = 0;
+    
+
+    CloseUART1();
+    ConfigIntUART1(UART_RX_INT_DIS & UART_TX_INT_DIS);
+    baud = 5;
+    
+    OpenUART1(UART_EN & UART_IDLE_CON & UART_IrDA_DISABLE & UART_MODE_FLOW
+        & UART_UEN_00 & UART_DIS_WAKE & UART_DIS_LOOPBACK
+        & UART_DIS_ABAUD & UART_UXRX_IDLE_ONE & UART_BRGH_SIXTEEN
+        & UART_NO_PAR_8BIT & UART_1STOPBIT,
+          UART_INT_TX_BUF_EMPTY & UART_IrDA_POL_INV_ZERO
+        & UART_SYNC_BREAK_DISABLED & UART_TX_ENABLE & UART_TX_BUF_NOT_FUL & UART_INT_RX_CHAR
+        & UART_ADR_DETECT_DIS & UART_RX_OVERRUN_CLEAR,
+          5);
+
+
+    OpenTimer1(T1_ON & T1_IDLE_STOP & T1_GATE_OFF & T1_PS_1_64 & T1_SYNC_EXT_OFF & T1_SOURCE_INT, 0x1FFF);
+    ConfigIntTimer1(T1_INT_PRIOR_2 & T1_INT_ON);
+
+    ConfigIntMCPWM1(PWM1_INT_DIS);
+    SetDCMCPWM1(1,0x0,0);
+    
+    period = 0x7;
+    sptime = 0x0;
+    config1 = (PWM1_EN & PWM1_IDLE_STOP & PWM1_OP_SCALE1 & PWM1_IPCLK_SCALE1 & PWM1_MOD_FREE);
+    config2 = (PWM1_MOD1_IND & PWM1_PDIS3H & PWM1_PDIS2H & PWM1_PEN1H & PWM1_PDIS3L & PWM1_PDIS2L & PWM1_PEN1L);
+    config3 = (PWM1_SEVOPS1 & PWM1_OSYNC_PWM & PWM1_UEN);
+    OpenMCPWM1(period,sptime,config1,config2,config3);
+
+
+
+    while(1)
+    {
+        WriteUART1(0x69);
+        while(BusyUART1());
+        __delay_ms(1);
+        
+    }
+
+}
---
+++ balisesIR/beacon.X/system.c
@@ -0,0 +1,52 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+/* Device header file */
+#if defined(__dsPIC33E__)
+	#include <p33Exxxx.h>
+#elif defined(__dsPIC33F__)
+	#include <p33Fxxxx.h>
+#endif
+
+#include <stdint.h>          /* For uint32_t definition                       */
+#include <stdbool.h>         /* For true/false definition                     */
+
+#include "system.h"          /* variables/params used by system.c             */
+
+/******************************************************************************/
+/* System Level Functions                                                     */
+/*                                                                            */
+/* Custom oscillator configuration funtions, reset source evaluation          */
+/* functions, and other non-peripheral microcontroller initialization         */
+/* functions get placed in system.c.                                          */
+/*                                                                            */
+/******************************************************************************/
+
+/* Refer to the device Family Reference Manual Oscillator section for
+information about available oscillator configurations.  Typically
+this would involve configuring the oscillator tuning register or clock
+switching useing the compiler's __builtin_write_OSCCON functions.
+Refer to the C Compiler for PIC24 MCUs and dsPIC DSCs User Guide in the
+compiler installation directory /doc folder for documentation on the
+__builtin functions.*/
+
+/* TODO Add clock switching code if appropriate.  An example stub is below.   */
+void ConfigureOscillator(void)
+{
+
+#if 0
+        /* Disable Watch Dog Timer */
+        RCONbits.SWDTEN = 0;
+
+        /* When clock switch occurs switch to Primary Osc (HS, XT, EC) */
+        __builtin_write_OSCCONH(0x02);  /* Set OSCCONH for clock switch */
+        __builtin_write_OSCCONL(0x01);  /* Start clock switching */
+        while(OSCCONbits.COSC != 0b011);
+
+        /* Wait for Clock switch to occur */
+        /* Wait for PLL to lock, only if PLL is needed */
+        /* while(OSCCONbits.LOCK != 1); */
+#endif
+}
+
