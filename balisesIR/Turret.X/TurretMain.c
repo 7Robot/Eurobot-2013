4c36705de3e78c2f953597e8c36c464c75f9fd3b
Ken <ken.hasselmann@etu.enseeiht.fr>
2013-04-24 15:45:24 +0200

    correction et clareté du code de turret

--- balisesIR/Turret.X/TurretMain.c
+++ balisesIR/Turret.X/TurretMain.c
@@ -66,18 +66,6 @@ int16_t main(void)
     /* Initialize IO ports and peripherals */
     InitApp();
 
-    TRISAbits.TRISA0 = 0; // Microstick LED
-    LATAbits.LATA0 = 1;   // affichage recepteur 1 - adversaire 1
-    TRISAbits.TRISA1 = 0;
-    LATAbits.LATA0 = 1;   // affichage recepteur 2 - adversaire 1
-    TRISBbits.TRISB0 = 0;
-    LATBbits.LATB1 = 1;   // affichage recepteur 1 - adversaire 2
-    TRISBbits.TRISB1 = 0;
-    LATBbits.LATB1 = 1;   // affichage recepteur 2 - adversaire 2
-    TRISCbits.TRISC9 = 1;  // TSOP1 RC9 non present sur 33FJ64MC802
-    TRISBbits.TRISB5 = 1;  // TSOP2
-	// ...
-
     int donnees[nombre_recepteurs*taille_uart];
     int recu = 0;
     
@@ -87,10 +75,10 @@ int16_t main(void)
     {
 	// Detection du bit de Start
         if ((TSOP1 == 0) || (TSOP2 == 0) || (TSOP3 == 0) || (TSOP4 == 0) || (TSOP5 == 0) || (TSOP6 == 0) || (TSOP7 == 0) || (TSOP8 == 0) || 
-            (TSOP9 == 0) || (TSOP10 == 0) || (TSOP11 == 0) || (TSOP12 == 0) || (TSOP13 == 0) || (TSOP14 == 0) || (TSOP15 == 0) || (TSOP16 == 0) || )
+            (TSOP9 == 0) || (TSOP10 == 0) || (TSOP11 == 0) || (TSOP12 == 0) || (TSOP13 == 0) || (TSOP14 == 0) || (TSOP15 == 0) || (TSOP16 == 0))
         {
             __delay_us(bit_periode_us/2); // Se place au millieu du bit de Start
-            acquisition(int donnees[]);
+            acquisition(donnees);
             __delay_us(bit_periode_us); // Sort de la trame envoyee
             recu = 1;
         }
@@ -106,6 +94,7 @@ int16_t main(void)
         }
     }
 }
+
 
 void acquisition(int donnees[])
 {
@@ -138,26 +127,47 @@ int comparer(int donnees[], int recepteur)
 	int egal = 1;
     for(i = 0 ; i < taille_uart && egal ; i++)
     {
-        if(donnees[recepteur*taille_uart+i] != adversaire1[i]){egal = 0;}
+        if(donnees[recepteur*taille_uart+i] != adversaire1[i])
+		{
+			egal = 0;
+		}
     }
-	if(i == taille_uart && egal) {adversaire = 1;}
+	if(i == taille_uart && egal) 
+	{
+		adversaire = 1;
+	}
 	// Detection Adversaire 2
 	int egal = 1;
     for(i = 0 ; i<taille_uart && egal ; i++)
     {
-        if(donnees[recepteur*taille_uart+i] != adversaire2[i]){egal = 0;}
+        if(donnees[recepteur*taille_uart+i] != adversaire2[i])
+		{
+			egal = 0;
+		}
     }
-	if(i == taille_uart && egal) {adversaire = 2;}
+	if(i == taille_uart && egal) 
+	{
+		adversaire = 2;
+	}
 	
     return adversaire;
 }
 
 void lissage(void)
 {
-	if((reperage[nombre_recepteurs-1] != 0) && (reperage[nombre_recepteurs-1] == reperage[1]) && (reperage[0] != reperage[nombre_recepteurs-1])) {reperage[0] = reperage[nombre_recepteurs-1];}
+	if((reperage[nombre_recepteurs-1] != 0) && (reperage[nombre_recepteurs-1] == reperage[1]) && (reperage[0] != reperage[nombre_recepteurs-1]))
+	{
+		reperage[0] = reperage[nombre_recepteurs-1];
+	}
 	for(i= 0 ; i < nombre_recepteurs-2 ; i++)
 	{
-		if((reperage[i] != 0) && (reperage[i] == reperage[i+2]) && (reperage[i+1] != reperage[i])) {reperage[i+1] = reperage[i];}
+		if((reperage[i] != 0) && (reperage[i] == reperage[i+2]) && (reperage[i+1] != reperage[i])) 
+		{
+			reperage[i+1] = reperage[i];
+		}
 	}
-	if((reperage[nombre_recepteurs-2] != 0) && (reperage[nombre_recepteurs-2] == reperage[0]) && (reperage[nombre_recepteurs-1] != reperage[nombre_recepteurs-2])) {reperage[nombre_recepteurs-1] = reperage[nombre_recepteurs-2];}
+	if((reperage[nombre_recepteurs-2] != 0) && (reperage[nombre_recepteurs-2] == reperage[0]) && (reperage[nombre_recepteurs-1] != reperage[nombre_recepteurs-2])) 
+	{
+		reperage[nombre_recepteurs-1] = reperage[nombre_recepteurs-2];
+	}
 }
