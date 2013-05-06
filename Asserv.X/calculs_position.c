ff55bee6f6b046bf354623d6e61dc9fdf29e5c7e
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-05-07 01:16:10 +0200

    oubli d’un fichier lors du fix du revert du merge

--- Asserv.X/calculs_position.c
+++ Asserv.X/calculs_position.c
@@ -1,7 +1,7 @@
 /*
 * Asserv dsPIC33F
 * Compiler : Microchip xC16
-* µC : 33FJ64MC802
+* µC : 33FJ64MC804
 * Avril 2013
 *    ____________      _           _
 *   |___  /| ___ \    | |         | |
@@ -21,7 +21,7 @@
 #include <math.h>
 
 
-    volatile float PosX = 0.0, PosY = 0.0, Theta = 0.0;
+volatile float PosX = 0.0, PosY = 0.0, Theta = 0.0;
 
 
 void Set_Position(float NewX, float NewY)           // permet une mise à jour de la position, du robot
@@ -30,69 +30,69 @@ void Set_Position(float NewX, float NewY)           // permet une mise à jour de
     PosY = NewY;
 }
 
- void Set_Postion_Angle(float NewX, float NewY, float NewTheta)  // remet totalement à jour la position
- {
-     Theta = NewTheta;
-     PosX = NewX;
-     PosY = NewY;
- }
+void Set_Postion_Angle(float NewX, float NewY, float NewTheta)  // remet totalement à jour la position
+{
+    Theta = NewTheta;
+    PosX = NewX;
+    PosY = NewY;
+}
 
- void Get_Position (float* X, float* Y, float* angle)       // renvoie la position actuelle du robot
- {
-     *X = PosX;
-     *Y = PosY;
-     *angle = Theta;
- }
+void Get_Position (float* X, float* Y, float* angle)       // renvoie la position actuelle du robot
+{
+    *X = PosX;
+    *Y = PosY;
+    *angle = Theta;
+}
 
- float Get_Angle(void)
- {
-     return Theta;
- }
+float Get_Angle(void)
+{
+    return Theta;
+}
 
- float Get_Consigne_Distance (float Consigne_PosX, float Consigne_PosY)            //renvoie la distance entre la consigne et la position actuelle
- {
-     return sqrt(pow(PosX - Consigne_PosX, 2) + pow(PosY - Consigne_PosY, 2));
- }
+float Get_Consigne_Distance (float Consigne_PosX, float Consigne_PosY)            //renvoie la distance entre la consigne et la position actuelle
+{
+    return sqrt(pow(PosX - Consigne_PosX, 2) + pow(PosY - Consigne_PosY, 2));
+}
 
- float Get_Consigne_Angle (float Consigne_PosX, float Consigne_PosY)             //renvoie l'angle entre la consigne et la position actuelle
- {
-     //return atan((Consigne_PosX - PosX)/(Consigne_PosY - PosY));
-     return atan2(Consigne_PosX - PosX, Consigne_PosY - PosY);
- }
+float Get_Consigne_Angle (float Consigne_PosX, float Consigne_PosY)             //renvoie l'angle entre la consigne et la position actuelle
+{
+    //return atan((Consigne_PosX - PosX)/(Consigne_PosY - PosY));
+    return atan2(Consigne_PosX - PosX, Consigne_PosY - PosY);
+}
 
- void Incremente_Position(int16_t Diff_D, int16_t Diff_G, volatile float *Vitesse, volatile float *Omega, volatile float *Distance, volatile float *Angle)
- {
-     float Avancement, Rotation;
+void Incremente_Position(int16_t Diff_D, int16_t Diff_G, volatile float *Vitesse, volatile float *Omega, volatile float *Distance, volatile float *Angle)
+{
+    float Avancement, Rotation;
 
-     Avancement = (float)((Diff_D * METER_BY_TICD) + (Diff_G * METER_BY_TICG)) * 0.5;       // distance en metres parcourue par le milieu du robot
-     *Vitesse = Avancement*100; // correpond à une vitesse en m/s: intervale de temps fixe (10ms)
-     Rotation = (float)(Diff_D * METER_BY_TICD - Diff_G * METER_BY_TICG)/ LARGEUR_ROBOT;    // delta theta du robot
-     *Omega = Rotation;         //correspond à une vitesse angulaire en ??
+    Avancement = (float)((Diff_D * METER_BY_TICD) + (Diff_G * METER_BY_TICG)) * 0.5;       // distance en metres parcourue par le milieu du robot
+    *Vitesse = Avancement*100; // correpond à une vitesse en m/s: intervale de temps fixe (10ms)
+    Rotation = (float)(Diff_D * METER_BY_TICD - Diff_G * METER_BY_TICG)/ LARGEUR_ROBOT;    // delta theta du robot
+    *Omega = Rotation;         //correspond à une vitesse angulaire en ??
 
-     *Distance += Avancement;
-     PosX += Avancement * sin(Theta);
-     PosY += Avancement * cos(Theta);
-     Theta += Rotation;
+    *Distance += Avancement;
+    PosX += Avancement * sin(Theta);
+    PosY += Avancement * cos(Theta);
+    Theta += Rotation;
 
-     *Angle = Theta;
-     //PosX = 0;
- }
-
-
- 
+    *Angle = Theta;
+    //PosX = 0;
+}
 
 
- /* plan plateau
-  * Y
-  * ^
-  * |
-  * |
-  * |
-  * |
-  * |
-  * |
-  * 0------------> X
-  *
-  *
-  *
-  */
+
+
+
+/* plan plateau
+ * Y
+ * ^
+ * |
+ * |
+ * |
+ * |
+ * |
+ * |
+ * 0------------> X
+ *
+ *
+ *
+ */
